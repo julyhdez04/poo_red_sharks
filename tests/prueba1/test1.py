0adc6cf441526f2205d3888adc380c73bbd757c1
Mushi0w0 <zs23013953@estudiantes.uv.mx>
2026-08-24 12:16:40 -0600

    Update test1.py
    Se puede registrar correo electronico.

--- tests/prueba1/test1.py
+++ tests/prueba1/test1.py
@@ -1,13 +1,15 @@
 def main():
-    nombre = input("Ingresa tu nombre: ")
-    edad = input("Ingresa tu edad: ")
+    nombre = input(" Ingresa tu nombre: ")
+    edad = input(" Ingresa tu edad: ")
+    correo_electronico = input(" Ingresa tu correo institucional: ")
 
-    datos = f"Nombre: {nombre}\nEdad: {edad}\n"
+    datos = f"Nombre: {nombre}\nEdad: {edad}\nCorreo: {correo_electronico}\n"
 
     with open("datos_persona.txt", "a", encoding="utf-8") as archivo:
         archivo.write(datos)
 
-    print(f"Tu nombre es {nombre} y tu edad es {edad}")
+    print(f" Tu nombre es {nombre} y tu edad es {edad} ")
+    print(f" Tu correo es {correo_electronico} ") 
     print("La información se ha almacenado en datos_persona.txt")
 
 if __name__ == "__main__":
